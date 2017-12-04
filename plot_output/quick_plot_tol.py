e6af24c897c4a7448f94edfa64046c15fa81ccb0
Chon Lok Lei <chonloklei@gmail.com>
2017-12-04 23:40:04 +0000

    Used direct output from TestCipaOHaraModelSolver.hpp

--- plot_output/quick_plot_tol.py
+++ plot_output/quick_plot_tol.py
@@ -4,11 +4,14 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-tol_err = np.loadtxt("tol_R_Chaste.txt", delimiter=',')
-tol = np.arange(6,6+10)
+tol_test = np.loadtxt("solver_tol_test.txt")
+tol = tol_test[:,0]
+log_tol = np.abs(np.log(tol))
+tol_err = tol_test[:,1:]
+del(tol_test)
 
-plt.plot(tol, np.log(tol_err[:,0]), 'o-', label='CellML/Chaste')
-plt.plot(tol, np.log(tol_err[:,1]), 'o--', label='C/R (CiPA)')
+plt.plot(log_tol, np.log(tol_err[:,0]), 'o-', label='CellML/Chaste')
+plt.plot(log_tol, np.log(tol_err[:,1]), 'o--', label='C/R (CiPA)')
 plt.xlabel('|log(tolerance)|')
 plt.ylabel('log(||state variable difference||)')
 plt.legend()
