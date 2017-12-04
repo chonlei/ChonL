4e2cb4aeeceea34157db6d74d4aa2bc1e363006d
Chon Lok Lei <chonloklei@gmail.com>
2017-12-04 23:03:26 +0000

    Plot: compare R and Chaste solvers as function of tolerance

---
+++ plot_output/quick_plot_tol.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+tol_err = np.loadtxt("tol_R_Chaste.txt", delimiter=',')
+tol = np.arange(6,6+10)
+
+plt.plot(tol, np.log(tol_err[:,0]), 'o-', label='CellML/Chaste')
+plt.plot(tol, np.log(tol_err[:,1]), 'o--', label='C/R (CiPA)')
+plt.xlabel('|log(tolerance)|')
+plt.ylabel('log(|difference|)')
+plt.savefig('solver_tol_test.png', bbox_inch='tight')
