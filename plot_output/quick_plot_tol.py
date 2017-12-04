af50ccd117d3bf0f74e92ac2965d2b9696d787db
Chon Lok Lei <chonloklei@gmail.com>
2017-12-04 23:41:49 +0000

    Used log base 10

--- plot_output/quick_plot_tol.py
+++ plot_output/quick_plot_tol.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 
 tol_test = np.loadtxt("solver_tol_test.txt")
 tol = tol_test[:,0]
-log_tol = np.abs(np.log(tol))
+log_tol = np.abs(np.log10(tol))
 tol_err = tol_test[:,1:]
 del(tol_test)
 
-plt.plot(log_tol, np.log(tol_err[:,0]), 'o-', label='CellML/Chaste')
-plt.plot(log_tol, np.log(tol_err[:,1]), 'o--', label='C/R (CiPA)')
+plt.plot(log_tol, np.log10(tol_err[:,0]), 'o-', label='CellML/Chaste')
+plt.plot(log_tol, np.log10(tol_err[:,1]), 'o--', label='C/R (CiPA)')
 plt.xlabel('|log(tolerance)|')
 plt.ylabel('log(||state variable difference||)')
 plt.legend()
